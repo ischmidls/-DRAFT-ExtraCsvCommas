0eaa39d8462c8d4294c81f20eed7ed5f51f9d188
ischmidls <izak.schmidlkofer@gmail.com>
2021-06-11 06:49:45 -0700

    Attempt commit to @ischmidls

--- graphic_csv.py
+++ graphic_csv.py
@@ -13,12 +13,15 @@ import PySimpleGUI as sg
 import os.path
 
 
-def query_up():
-    layout = [[sg.Text('Enter output name: ')],
+def query_window(window, layout):
+    """
+    pop up window to name output file
+    """
+    new_layout = [[sg.Text('Enter output name: ')],
               [sg.InputText(key='-IN-')],
               [sg.Submit(), sg.Cancel()]]
 
-    window = sg.Window('Window Title', layout)
+    window[layout].update(new_layout)
 
     event, values = window.read()
     window.close()
@@ -26,7 +29,11 @@ def query_up():
     return values['-IN-']
 
 
+
 def event_folder(values, window):
+    """
+    return txt file names from selected folder
+    """
     folder =values["-FOLDER-"]
     try:
         # Get list of files in folder
@@ -44,7 +51,9 @@ def event_folder(values, window):
 
 
 def event_loop(window):
-    # Create an event loop
+    """
+    create an event loop
+    """
     while True:
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
@@ -53,25 +62,26 @@ def event_loop(window):
             event_folder(values, window)
         if event == "-FILE LIST-":
             os.chdir(values["-FOLDER-"])
-            o_file = query_up()
+            o_file = query_window()
             cc.Spaces(values["-FILE LIST-"][0]).space_comma(o_file)
 
 
 def main():
     layout = [
         [
-            sg.Text("Files"),
+            sg.Text("Pick a file path: "),
             sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
             sg.FolderBrowse(),
         ],
         [
+            sg.Text("Pick a file to separate: "),
             sg.Listbox(values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"),
         ]
 
     ]
 
     # Create the window
-    window = sg.Window(title="Point of View", layout=layout, margins=(300, 150))
+    window = sg.Window(title="Comma Separated Values", layout=layout, margins=(300, 150))
 
     event_loop(window)
 
